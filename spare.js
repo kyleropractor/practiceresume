5e5226aa1cc1a5624d1eff21b9cbc0b26b4c3397
Kyle Steineman <kmsteineman@gmail.com>
2017-02-02 10:51:10 -0600

    Delete spare.js

--- spare.js
+++
@@ -1,6 +0,0 @@
-        var i = 0;
-        for(i;i < 10;i++) {
-        var formattedImage = HTMLprojectImage.replace(data, project.images[i]);
-        $(".project-entry:last").append(formattedImage);
-              if (project.images[i].length < 0) { break; };
-      };
